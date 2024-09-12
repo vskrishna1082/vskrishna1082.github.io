807cf1ac7e4957c887f26d2b4a5445df6914f60d
Krishna Iyer V S <krishnaiiserp@gmail.com>
2024-09-12 17:32:22 +0200

    Better living cat

--- assets/js/schrodinger.js
+++ assets/js/schrodinger.js
@@ -1,7 +1,7 @@
 dead_div = document.getElementById("dead-cat");
 living_div = document.getElementById("living-cat");
 
-if (Math.random()<=0.25) {
+if (Math.random()<=0.40) {
 dead_div.style.display = "none";
 living_div.style.display = "inline";
 }
